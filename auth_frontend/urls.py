3509a054e6adc142c09dd0040f42c51df3d5e0c1
ductt02 <20520454@gm.uit.edu.vn>
2022-10-25 08:45:22 +0700

    Fix bug routing login/signup

--- auth_frontend/urls.py
+++ auth_frontend/urls.py
@@ -3,6 +3,6 @@ from .views import LoginForm
 from .views import RegisterForm
 
 urlpatterns = [
-    path("loginForm", LoginForm.as_view(), name="loginForm"),
-    path("registerForm", RegisterForm.as_view(), name="registerForm"),
+    path("login/", LoginForm.as_view(), name="login"),
+    path("signup/", RegisterForm.as_view(), name="signup"),
 ]
