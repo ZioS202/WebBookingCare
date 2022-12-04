c7cc7f7edac47df2c256fafd8b6e76394e32b25c
daemon104 <88297957+daemon104@users.noreply.github.com>
2022-12-04 20:22:30 +0700

    Update detaildoctor.html

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from django.contrib.auth.models import User
+from .models import Booking, Payment
+from doctors.models import DetailsDoctor, Schedule
 
 # Create your views here.
 
