449735fdca33e02d0e66e313ea09f2726500397a
daemon104 <88297957+daemon104@users.noreply.github.com>
2022-12-04 16:02:13 +0700

    Update doctor
    update urls.py, views.py

--- doctors/views.py
+++ doctors/views.py
@@ -1,16 +1,35 @@
+from django.views.generic import TemplateView, DetailView
 from http.client import ImproperConnectionState
 from operator import imod
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import DetailsDoctor, Schedule
+from clinics.models import Clinic
+from specialists.models import Specialist
+
+
 # Create your views here.
 
 def indexDoctor(request):
     return render(request,'doctors/base.html')
+
 def findDoctor(request):
-    return render(request,'doctors/FindDoctor.html')
+    doctor_list = DetailsDoctor.objects.all()
+    return render(request,'doctors/FindDoctor.html', {'doctor_list': doctor_list})
 
-def detailDoctor(request):
-    return render(request,'doctors/DetailDoctor.html')
+# def detailDoctor(request):
+#     return render(request,'doctors/DetailDoctor.html')
 
-def manageClinic(request):
-    return render(request, 'doctors/manageClinic.html')
+class DoctorDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['specialists'] = Specialist.objects.all()
+        context['clinic'] = Clinic.objects.all()
+        context['schedule'] = Schedule.objects.all()
+        return context
+    template_name = "doctors/DetailDoctor.html"
+    model = DetailsDoctor
+    context_object_name = "doctor"
+
+# def manageClinic(request):
+#     return render(request, 'doctors/manageClinic.html')
